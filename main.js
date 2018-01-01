60a7baeb692b62a800ad6ac0cd8661dd15cb797a
Hsinhan <bmhiamso1@gmail.com>
2018-01-01 22:53:17 +0800

    完成下載邏輯

--- main.js
+++ main.js
@@ -5,11 +5,24 @@ const path = require('path');
 //get info
 async function downloadPatchs() {
   //download info
-  const latestVer = await promiseDownload(
-    'https://raw.githubusercontent.com/iamso1/patchs-server/master/info.json',
-    'info.json'
+  const patchInfo = JSON.parse(
+    await promiseDownload(
+      'https://raw.githubusercontent.com/iamso1/patchs-server/master/info.json',
+      'sys/info.json'
+    )
   );
-  //
+  //get current patch version
+  const latestVer = patchInfo.version;
+
+  //get target service version
+  const target_version = JSON.parse(fs.readFileSync('', 'utf8')).version;
+
+  //if target version is older than latestVer, download all patchs to patchs folder
+  if (latestVer > target_version) {
+    for (let i = 0; i < patchInfo.urls.length; i++) {
+      promiseDownload(patchInfo.urls[i], `patches/ ${patchInfo.filenames[i]}`);
+    }
+  }
 }
 
 function promiseDownload(src, filename, directory = '') {
